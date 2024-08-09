868db764f1903479a4894fb2a3fea6a3be9915bd
Berkay <berkay.kucukkilavuz@gmail.com>
2024-08-09 13:33:25 +0100

    model and database updated

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,10 @@
 # admin.py
 
 from django.contrib import admin
-from .models import Category, Item, Basket
+from .models import Category, Item, Basket, Option
 
 admin.site.register(Category)
 admin.site.register(Item)
 admin.site.register(Basket)
+admin.site.register(Option)
 
--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ class Item(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     isAvailable = models.BooleanField(default=1)
     photo = models.ImageField(null=True)
-    options = models.CharField(max_length=100, blank=True, null=True) #this needs to be well defined
+    #options = models.CharField(max_length=100, blank=True, null=True) #this needs to be well defined
     description = models.TextField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
     created_at = models.DateTimeField(default=timezone.now)
@@ -36,7 +36,7 @@ class Option(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.option_name
+        return f'{self.parent_menu_item.name} - {self.option_name}'
 
 
 class Basket(models.Model):
@@ -45,8 +45,8 @@ class Basket(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     option = models.ForeignKey(Option, on_delete=models.CASCADE, null=True, blank=True)
     #menu_item_option = models.ForeignKey(MenuItemOption, on_delete=models.CASCADE, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return f"{self.item} ({self.quantity})"
