9ac29ddbf0d3c62fc1912922c569275b2e4f224d
Berkay <berkay.kucukkilavuz@gmail.com>
2024-08-09 12:44:36 +0100

    previous db commit and models updated -no migration

--- orders/models.py
+++ orders/models.py
@@ -26,10 +26,28 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
+
+class Option(models.Model):
+    parent_menu_item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    option_name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.option_name
+
+
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE, null=True, blank=True)
+    #menu_item_option = models.ForeignKey(MenuItemOption, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.item} ({self.quantity})"
+        #return f'{self.user.first_name}\'s Basket'
