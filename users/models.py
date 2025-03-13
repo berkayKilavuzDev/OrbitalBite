6b10c086e7032e08b7fea47561052424481fa585
Honorius_646 <onur.can190527@gmail.com>
2025-03-13 22:17:19 +0000

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -1,26 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    ROLE_CHOICES = [
-    ('admin', 'Admin'),
-    ('employee', 'Employee'),
-    ('customer', 'Customer'),
-    ]
-
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='users_groups',  # Django varsayılanından farklı bir isim
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='users_permissions',  # Django varsayılanından farklı bir isim
-        blank=True
-    )
-    def __str__(self):
-        return f"{self.username} - {self.get_role_display()}"  
+# Create your models here.
