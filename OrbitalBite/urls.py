7ae27fdd1ccc53539ca1ddef896774f43b8deafa
Honorius_646 <onur.can190527@gmail.com>
2025-03-13 16:41:02 +0000

    Add files via upload

--- OrbitalBite/urls.py
+++ OrbitalBite/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),    
-    path('', include('core.urls', namespace='core')), # Include URLs from the 'core' app
-    
-    path('', include('users.urls', namespace='users')), 
-    path('', include('orders.urls', namespace='orders')), 
-    path('', include('menu.urls', namespace='menu')),  
-]
+    path('admin/', admin.site.urls),
 
+    # Kullanıcı işlemleri (login, logout, register, profil güncelleme)
+    path('users/', include('users.urls')),  
+
+    # Menü işlemleri (ürün listesi, detaylar)
+    path('menu/', include('menu.urls')),
+
+    # Sipariş işlemleri (sipariş oluşturma, sipariş geçmişi)
+    path('orders/', include('orders.urls')),
+
+    # Ana sayfa ve genel işlemler
+    path('', include('core.urls')),  
+]
